f8c649f4cb82dea81efb45a05ef3232c7e2121e1
nikhilrajpatidar7-star <nikhilrajpatidar7@gmail.com>
2026-05-27 16:12:27 +0530

    Sorting

--- sorting.py
+++ sorting.py
@@ -56,18 +56,8 @@
 # print(new_list)
 
 
-# my_list = [10,8,6,4,2]
-# new_list = my_list[2:]
-# print(new_list)
-
-
 my_list = [10,8,6,4,2]
-new_list = my_list[-1:1]
+new_list = my_list[2:]
 print(new_list)
-del my_list[1:3]
-print(my_list)
-#del my_List
-del my_list[:]
-print(my_list)
 
 
