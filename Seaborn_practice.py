56257c84fe320d2b8441e404eec162683036c71f
nikhilrajpatidar7-star <nikhilrajpatidar7@gmail.com>
2026-06-18 16:21:26 +0530

    Seaborn

--- Seaborn_practice.py
+++ Seaborn_practice.py
@@ -67,22 +67,22 @@
 
 
 
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# import pandas as pd
-# import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
 
-# np.random.seed(42)
-# #Data
-# df = pd.DataFrame({
-#     'marks':   np.random.randint(40,100,100),
-#     'study_hours':   np.random.uniform(2,10,100),
-#     'city':   np.random.choice(['Bhopal','Indore','Jabalpur'],100),
-#     'gender':   np.random.choice(['Male','Female'],100)})
+np.random.seed(42)
+#Data
+df = pd.DataFrame({
+    'marks':   np.random.randint(40,100,100),
+    'study_hours':   np.random.uniform(2,10,100),
+    'city':   np.random.choice(['Bhopal','Indore','Jabalpur'],100),
+    'gender':   np.random.choice(['Male','Female'],100)})
 
-# #Pair plot - all relationship at once
-# sns.pairplot(df[['marks','study_hours']],diag_kind='kde')
-# plt.show()
+#Pair plot - all relationship at once
+sns.pairplot(df[['marks','study_hours']],diag_kind='kde')
+plt.show()
 
 
 
